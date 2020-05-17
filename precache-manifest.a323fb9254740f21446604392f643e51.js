3e11b26e5de162761d4b1e62fec710bafd9c85d1
singh1029 <60672804+singh1029@users.noreply.github.com>
2020-05-16 23:47:19 -0400

    Updates

---
+++ precache-manifest.a323fb9254740f21446604392f643e51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916bd86966c312cc7976528c3998c60a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0fe9a3a938b03f927c27",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0da6c84932a63cbbea19",
+    "url": "/portfolio/static/js/2.77945aaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.77945aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe9a3a938b03f927c27",
+    "url": "/portfolio/static/js/main.2321ff79.chunk.js"
+  },
+  {
+    "revision": "e345a16de91e7c06ddb7",
+    "url": "/portfolio/static/js/runtime-main.1879ff2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
